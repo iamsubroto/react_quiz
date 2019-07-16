bbc2b825d085c1594bc56aad06f2721f1cf2f117
Subroto Biswas <smartcoder500@gmail.com>
2019-07-16 14:45:58 +0530

    Updates

---
+++ precache-manifest.1e4f7701557d7db089102c4553f7144c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24efd751f321f229a1aef77fb4facb90",
+    "url": "/react_quiz/index.html"
+  },
+  {
+    "revision": "5b55cc77c9b35f2f578a",
+    "url": "/react_quiz/static/css/main.9d7d2b0a.chunk.css"
+  },
+  {
+    "revision": "eb07aa18f44788163eb8",
+    "url": "/react_quiz/static/js/2.81feea38.chunk.js"
+  },
+  {
+    "revision": "5b55cc77c9b35f2f578a",
+    "url": "/react_quiz/static/js/main.e25c6d7e.chunk.js"
+  },
+  {
+    "revision": "d5d57b4f0bb2c9a3888d",
+    "url": "/react_quiz/static/js/runtime~main.38265c6e.js"
+  },
+  {
+    "revision": "7960384844b9f8852d6e2260b8956953",
+    "url": "/react_quiz/static/media/start.79603848.png"
+  }
+]);
